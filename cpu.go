fd403348db96ac3507badde9245d4b61772064ae
Nicholas Ulricksen <n.ulricksen@gmail.com>
2021-05-18 02:44:58 -0700

    implement some addressing modes (implied, zero-page, indexed indirect) and some instructions (ASL, BRK, ORA)

--- cpu.go
+++ cpu.go
@@ -1,5 +1,9 @@
 package nes
 
+import (
+	"fmt"
+)
+
 type Cpu6502 struct {
 	Pc     uint16 // Program Counter
 	Sp     byte   // Stack Pointer: low 8 bits of next free location on stack.
@@ -10,23 +14,70 @@ type Cpu6502 struct {
 
 	bus *Bus // Communication Bus
 
+	// Internal variables
+	cycles        byte   // Remaining cycles for current insturction
+	opcode        byte   // Opcode representing next instruction to be executed
+	addrAbs       uint16 // Set by addressing mode functions, used by instructions
+	fetched       byte   // Byte of memory used by CPU instructions
+	isImpliedAddr bool   // Whether the current instruction's address mode is implied
+	cycleCount    uint32 // Total # of cycles executed by the CPU
+
 	instLookup [16 * 16]Instruction // Instruction operation lookup
 }
+
+const stackBase uint16 = 0x0100
 
 func NewCpu6502() *Cpu6502 {
 	cpu := &Cpu6502{
 		Pc:     0x0000,
-		Sp:     0x00,
+		Sp:     0xFD,
 		A:      0x00,
 		X:      0x00,
 		Y:      0x00,
 		Status: 0x00,
+
+		cycles:        0,
+		opcode:        0x00,
+		addrAbs:       0x0000,
+		fetched:       0x00,
+		isImpliedAddr: false,
+		cycleCount:    0,
 	}
 
 	// Create the lookup table containing all the CPU instructions.
 	// Reference: http://archive.6502.org/datasheets/rockwell_r650x_r651x.pdf
 	cpu.instLookup = [16 * 16]Instruction{
-		{"BRK", cpu.amIMP, cpu.opBRK, 7}, {"ORA", cpu.amIZX, cpu.opORA, 6},
+		{"BRK", cpu.opBRK, cpu.amIMP, 7}, {"ORA", cpu.opORA, cpu.amIZX, 6}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"ORA", cpu.opORA, cpu.amZP0, 3}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"ORA", cpu.opORA, cpu.amZP0, 3}, {"ASL", cpu.opASL, cpu.amZP0, 5}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
+
+		{"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2}, {"XXX", cpu.opXXX, cpu.amIMP, 2},
 	}
 
 	return cpu
@@ -43,6 +94,22 @@ func (cpu *Cpu6502) read(addr uint16) byte {
 // Write to the attached bus.
 func (cpu *Cpu6502) write(addr uint16, data byte) {
 	cpu.bus.Write(addr, data)
+}
+
+// Read a word from memory (little endian order).
+func (cpu *Cpu6502) readWord(addr uint16) uint16 {
+	lo := cpu.read(addr)
+	hi := cpu.read(addr + 1)
+
+	return (uint16(hi) << 8) | uint16(lo)
+}
+
+// Read a byte from memory at the address previously set by the appropriate
+// addressing mode function. Avoid if current instruction's address mode is implied.
+func (cpu *Cpu6502) fetch() {
+	if !cpu.isImpliedAddr {
+		cpu.fetched = cpu.read(cpu.addrAbs)
+	}
 }
 
 ////////////////////////////////////////////////////////////////
@@ -74,27 +141,126 @@ func (cpu *Cpu6502) setFlag(f SF6502, b bool) {
 }
 
 ////////////////////////////////////////////////////////////////
+// Interrupts
+const resetVectAddr = 0xFFFC
+const irqVectAddr = 0xFFFE
+
+func (cpu *Cpu6502) Reset() {
+	// TODO: set the program counter to the absolute address read from location
+	// 0xFFFC.
+
+	// Clear registers, reset stack pointer
+	cpu.A = 0x00
+	cpu.X = 0x00
+	cpu.Y = 0x00
+	cpu.Status = 0x00 | byte(StatusFlagX)
+	cpu.Sp = 0xFD
+
+	// Get the program counter from the reset vector location in RAM.
+	cpu.Pc = cpu.readWord(resetVectAddr)
+
+	// TODO: clear internal variables (absolute/relative addresses, fetched)
+
+	// Spend time on reset
+	cpu.cycles = 8
+}
+
+// Interrupt Request
+func (cpu *Cpu6502) IRQ() {}
+func (cpu *Cpu6502) NMI() {}
+
+// Cycle represents one CPU clock cycle.
+func (cpu *Cpu6502) Cycle() {
+	if cpu.cycles == 0 {
+		// Get the next opcode by reading from the bus at the location of the
+		// current program counter.
+		cpu.opcode = cpu.read(cpu.Pc)
+
+		// Lookup by opcode the instruction to be executed.
+		inst := cpu.instLookup[cpu.opcode]
+
+		fmt.Printf("from %#x fetched %#x: %v\n", cpu.Pc, cpu.opcode, inst)
+
+		cpu.Pc++
+
+		// Set required cycles for instruction execution.
+		cpu.cycles = inst.Cycles
+
+		// Add any additional cycles needed by either the addressing mode or
+		// instruction.
+		extraCycles1 := inst.AddrMode()
+		extraCycles2 := inst.Execute()
+
+		cpu.cycles += (extraCycles1 & extraCycles2)
+	}
+
+	// Turn implied address mode off, just in case the last instruction turned it on.
+	cpu.isImpliedAddr = false
+
+	cpu.cycleCount++
+
+	cpu.cycles--
+}
+
+////////////////////////////////////////////////////////////////
 // Addressing Modes
 // These functions return any extra cycles needed for execution.
-func (cpu *Cpu6502) amIMP() byte { return 0x00 }
+
+// Implied:
+func (cpu *Cpu6502) amIMP() byte {
+	cpu.isImpliedAddr = true
+
+	cpu.fetched = cpu.A
+	return 0x00
+}
+
 func (cpu *Cpu6502) amIMM() byte { return 0x00 }
 func (cpu *Cpu6502) amREL() byte { return 0x00 }
-func (cpu *Cpu6502) amZP0() byte { return 0x00 }
+
+func (cpu *Cpu6502) amZP0() byte {
+	// Use the second byte of the instruction to index into page zero.
+	lo := cpu.read(cpu.Pc)
+	cpu.Pc++
+
+	cpu.addrAbs = uint16(lo)
+
+	return 0x00
+}
+
 func (cpu *Cpu6502) amZPX() byte { return 0x00 }
 func (cpu *Cpu6502) amZPY() byte { return 0x00 }
 func (cpu *Cpu6502) amABS() byte { return 0x00 }
 func (cpu *Cpu6502) amABX() byte { return 0x00 }
 func (cpu *Cpu6502) amABY() byte { return 0x00 }
 func (cpu *Cpu6502) amIND() byte { return 0x00 }
-func (cpu *Cpu6502) amIZX() byte { return 0x00 }
+
+// Indexed Indirect:
+func (cpu *Cpu6502) amIZX() byte {
+	// Add the second byte of the instruction with the contents of register X.
+	// This result is a zero page memory location pointing to the low order byte
+	// of the effective address. The next memory location contains the high
+	// order byte. Both memory locations must be in page zero.
+
+	// Get the low order byte of the address.
+	addr := cpu.read(cpu.Pc) + cpu.X
+	cpu.Pc++
+
+	// Read effective address from page zero.
+	lo := cpu.read(uint16(addr))
+	hi := cpu.read((uint16(addr) + 1) % 0x0100) // Zero page wraparound
+	cpu.addrAbs = uint16(hi)<<8 | uint16(lo)
+
+	return 0x00
+}
+
 func (cpu *Cpu6502) amIZY() byte { return 0x00 }
 
 ////////////////////////////////////////////////////////////////
 // Instructions
 type Instruction struct {
 	Name     string
-	AddrMode func() byte
 	Execute  func() byte
+	AddrMode func() byte
 	Cycles   byte
 }
 
@@ -102,7 +268,34 @@ type Instruction struct {
 // cycles necessary for execution.
 func (cpu *Cpu6502) opADC() byte { return 0x00 }
 func (cpu *Cpu6502) opAND() byte { return 0x00 }
-func (cpu *Cpu6502) opASL() byte { return 0x00 }
+
+// ASL - Arithmetic Shift Left
+func (cpu *Cpu6502) opASL() byte {
+	cpu.fetch()
+
+	// Set carry flag to old bit 7.
+	cpu.setFlag(StatusFlagC, cpu.fetched&(1<<7) > 0)
+
+	result := cpu.fetched << 1
+
+	// Write result to accumulator register if in implied addressing mode, else
+	// write to addrAbs location in memory.
+	if cpu.isImpliedAddr {
+		cpu.A = result
+	} else {
+		cpu.write(cpu.addrAbs, result)
+	}
+
+	cpu.setFlag(StatusFlagZ, cpu.A == 0)
+
+	// Set negative flag if bit 7 of result is set.
+	if result&(1<<7) > 0 {
+		cpu.setFlag(StatusFlagN, true)
+	}
+
+	return 0x00
+}
+
 func (cpu *Cpu6502) opBCC() byte { return 0x00 }
 func (cpu *Cpu6502) opBCS() byte { return 0x00 }
 func (cpu *Cpu6502) opBEQ() byte { return 0x00 }
@@ -110,7 +303,30 @@ func (cpu *Cpu6502) opBIT() byte { return 0x00 }
 func (cpu *Cpu6502) opBMI() byte { return 0x00 }
 func (cpu *Cpu6502) opBNE() byte { return 0x00 }
 func (cpu *Cpu6502) opBPL() byte { return 0x00 }
-func (cpu *Cpu6502) opBRK() byte { return 0x00 }
+
+// BRK - Force Interrupt
+func (cpu *Cpu6502) opBRK() byte {
+	// Push the high byte of the program counter to the stack.
+	cpu.write(stackBase+uint16(cpu.Sp), byte((cpu.Pc>>8)&0xFF))
+	cpu.Sp--
+
+	// Push the low byte of the program counter to the stack.
+	cpu.write(stackBase+uint16(cpu.Sp), byte(cpu.Pc)&0xFF)
+	cpu.Sp--
+
+	// Push the CPU status to the stack.
+	cpu.write(stackBase+uint16(cpu.Sp), cpu.Status)
+	cpu.Sp--
+
+	// Load the IRQ interrupt vector at $FFFE/F to the PC.
+	cpu.Pc = cpu.readWord(irqVectAddr)
+
+	// Set break flag to 1.
+	cpu.setFlag(StatusFlagB, true)
+
+	return 0x00
+}
+
 func (cpu *Cpu6502) opBVC() byte { return 0x00 }
 func (cpu *Cpu6502) opBVS() byte { return 0x00 }
 func (cpu *Cpu6502) opCLC() byte { return 0x00 }
@@ -134,7 +350,19 @@ func (cpu *Cpu6502) opLDX() byte { return 0x00 }
 func (cpu *Cpu6502) opLDY() byte { return 0x00 }
 func (cpu *Cpu6502) opLSR() byte { return 0x00 }
 func (cpu *Cpu6502) opNOP() byte { return 0x00 }
-func (cpu *Cpu6502) opORA() byte { return 0x00 }
+
+// ORA - Logical Inclusive OR
+func (cpu *Cpu6502) opORA() byte {
+	cpu.fetch()
+
+	cpu.A |= cpu.fetched
+
+	cpu.setFlag(StatusFlagZ, cpu.A == 0x00)      // if A == 0
+	cpu.setFlag(StatusFlagN, (cpu.A&(1<<7) > 0)) // if bit 7 set
+
+	return 0x00
+}
+
 func (cpu *Cpu6502) opPHA() byte { return 0x00 }
 func (cpu *Cpu6502) opPHP() byte { return 0x00 }
 func (cpu *Cpu6502) opPLA() byte { return 0x00 }
@@ -156,3 +384,6 @@ func (cpu *Cpu6502) opTSX() byte { return 0x00 }
 func (cpu *Cpu6502) opTXA() byte { return 0x00 }
 func (cpu *Cpu6502) opTXS() byte { return 0x00 }
 func (cpu *Cpu6502) opTYA() byte { return 0x00 }
+
+// Catch-all instruction for illegal opcodes.
+func (cpu *Cpu6502) opXXX() byte { return 0x00 }
