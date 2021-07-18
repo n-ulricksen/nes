387cc33ac8bb0513796a5a47455adcddd751aad7
Nicholas Ulricksen <n.ulricksen@gmail.com>
2021-07-18 08:05:08 -0700

    function time tracking

---
+++ nes/util.go
@@ -0,0 +1,27 @@
+package nes
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"runtime"
+	"time"
+)
+
+// Function time tracking thanks to:
+// https://stackoverflow.com/questions/45766572/is-there-an-efficient-way-to-calculate-execution-time-in-golang
+func TimeTrack(start time.Time) {
+	elapsed := time.Since(start)
+
+	// Skip this function, and fetch the PC and file for its parent.
+	pc, _, _, _ := runtime.Caller(1)
+
+	// Retrieve a function object this functions parent.
+	funcObj := runtime.FuncForPC(pc)
+
+	// Regex to extract just the function name (and not the module path).
+	runtimeFunc := regexp.MustCompile(`^.*\.(.*)$`)
+	name := runtimeFunc.ReplaceAllString(funcObj.Name(), "$1")
+
+	log.Println(fmt.Sprintf("%s took %s", name, elapsed))
+}
