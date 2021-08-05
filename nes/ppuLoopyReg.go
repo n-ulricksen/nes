1d162f3b0294b5dbb6cb16fdca7f015615a2c5ab
Nicholas Ulricksen <n.ulricksen@gmail.com>
2021-08-05 15:15:47 -0700

    remove unnecessary type

--- nes/ppuLoopyReg.go
+++ nes/ppuLoopyReg.go
@@ -10,13 +10,12 @@ package nes
 //   YYYYY - coarse Y scroll
 //   XXXXX - coarse X scroll
 type PpuLoopyReg uint16
-type PpuLoopyFlagset uint16
 
 const (
-	loopyCoarseX   PpuLoopyFlagset = 0b11111
-	loopyCoarseY                   = 0b11111 << 5
-	loopyNametable                 = 0b11 << 10
-	loopyFineY                     = 0b111 << 12
+	loopyCoarseX   PpuLoopyReg = 0b11111
+	loopyCoarseY               = 0b11111 << 5
+	loopyNametable             = 0b11 << 10
+	loopyFineY                 = 0b111 << 12
 )
 
 // Returns the value fo the loopy register as a unsigned 16-bit integer.
@@ -28,50 +27,58 @@ func (r *PpuLoopyReg) value() uint16 {
 // given value.
 func (r *PpuLoopyReg) setCoarseX(val byte) {
 	// Get relevant 5 bits
-	setBits := uint16(val) & 0b11111
+	setBits := PpuLoopyReg(val) & 0b11111
 
 	// Clear bits about to be set
-	*r &^= PpuLoopyReg(loopyCoarseX)
+	*r &^= loopyCoarseX
 
 	// Set new bits
-	*r |= PpuLoopyReg(setBits)
+	*r |= (setBits)
 }
 
 // Sets coarse Y (bits 5-9) of the loopy register with the low 5 bits of the
 // given value.
 func (r *PpuLoopyReg) setCoarseY(val byte) {
 	// Get relevant 5 bits
-	setBits := uint16(val) & 0b11111
+	setBits := PpuLoopyReg(val) & 0b11111
 
 	// Clear bits about to be set
-	*r &^= PpuLoopyReg(loopyCoarseY)
+	*r &^= loopyCoarseY
 
 	// Set new bits
-	*r |= PpuLoopyReg(setBits << 5)
+	*r |= (setBits << 5)
 }
 
 // Sets nametable (bits 10-11) of the loopy register with the low 2 bits of the
 // given value.
 func (r *PpuLoopyReg) setNametable(val byte) {
 	// Get relevant 2 bits
-	setBits := uint16(val) & 0b11
+	setBits := PpuLoopyReg(val) & 0b11
 
 	// Clear bits about to be set
-	*r &^= PpuLoopyReg(loopyNametable)
+	*r &^= loopyNametable
 
 	// Set new bits
-	*r |= PpuLoopyReg(setBits << 10)
+	*r |= (setBits << 10)
 }
 
 // Sets fine Y (bits 12-14) of the loopy register with the low 3 bits of the
 // given value.
 func (r *PpuLoopyReg) setFineY(val byte) {
 	// Get relevant 3 bits
-	setBits := uint16(val) & 0b111
+	setBits := PpuLoopyReg(val) & 0b111
 
 	// Clear bits about to be set
-	*r &^= PpuLoopyReg(loopyFineY)
+	*r &^= loopyFineY
 
 	// Set new bits
-	*r |= PpuLoopyReg(setBits << 12)
+	*r |= (setBits << 12)
 }
+
+func (r *PpuLoopyReg) getCoarseX() byte {
+	return byte(*r & PpuLoopyReg(loopyCoarseX))
+}
+
+func (r *PpuLoopyReg) getCoarseY() byte   { return 0 }
+func (r *PpuLoopyReg) getNametable() byte { return 0 }
+func (r *PpuLoopyReg) getFineY() byte     { return 0 }
