7481c7d57965e18c140c2deb917a888faac6b748
Nicholas Ulricksen <n.ulricksen@gmail.com>
2022-03-18 11:43:24 -0700

    remove old dead code

--- main.go
+++ main.go
@@ -3,15 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 
 	"github.com/n-ulricksen/nes-emulator/nes"
 
-	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/faiface/pixel/text"
-	"golang.org/x/image/colornames"
-	"golang.org/x/image/font/basicfont"
 )
 
 // Command line flags
@@ -37,7 +33,6 @@ func main() {
 	fmt.Println("Resetting NES...")
 	nesEmulator.Cpu.Reset()
 
-	//startDebugMode(nesEmulator)
 	pixelgl.Run(nesEmulator.Run)
 }
 
@@ -46,93 +41,6 @@ func parseFlags() {
 	flag.BoolVar(&flagLogging, "l", false, "enable logging")
 
 	flag.Parse()
-}
-
-func startDebugMode(nesEmu *nes.Bus) {
-	// Channel used to signal CPU to cycle.
-	cycleChan := make(chan bool)
-
-	// Function to run the NES emulator. This must be run on a separate goroutine
-	// due to PixelGL requiring main.
-	go func(nesEmu *nes.Bus, cycleChan <-chan bool) {
-		// There may be cycles left to complete for current instruction/interrupt.
-		cycles := int(nesEmu.Cpu.Cycles)
-
-		for {
-			// While debug mode is running, we wait for signals from the PixelGL
-			// subroutine to trigger CPU cycles. Each signal on the channel will
-			// cause 1 CPU instruction worth of cycles.
-			select {
-			case <-cycleChan:
-				if cycles == 0 {
-					cycles = int(nesEmu.Cpu.InstLookup[nesEmu.CpuRead(nesEmu.Cpu.Pc)].Cycles)
-				}
-
-				for i := 0; i < cycles; i++ {
-					nesEmu.Clock()
-				}
-				cycles = 0
-				nesEmu.CheckForNestestErrors()
-			}
-		}
-	}(nesEmu, cycleChan)
-
-	// PixelGL must run on main thread.
-	pixelgl.Run(RunDebugWindow(nesEmu, cycleChan))
-}
-
-func RunDebugWindow(nesEmu *nes.Bus, cycleChan chan<- bool) func() {
-	return func() {
-		// Config
-		cfg := pixelgl.WindowConfig{
-			Title:  "NES Emulator Debug",
-			Bounds: pixel.R(0, 0, 1024, 768),
-			VSync:  true,
-		}
-
-		// Create new window.
-		win, err := pixelgl.NewWindow(cfg)
-		if err != nil {
-			log.Fatal("Unable to create new PixelGL window...\n", err)
-		}
-
-		// Create atlas and text boxes.
-		basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-
-		ramText := text.New(pixel.V(10, 768-20), basicAtlas)
-		regText := text.New(pixel.V(400, 768-20), basicAtlas)
-
-		// Main display (the running game)
-		// XXX: Might need to use a different graphics library
-
-		isAutoRun := false
-
-		// Draw to the screen
-		for !win.Closed() {
-			win.Clear(colornames.Black)
-
-			ramText.Clear()
-			printDebugMem(ramText, nesEmu)
-			ramText.Draw(win, pixel.IM.Scaled(ramText.Orig, 1))
-
-			regText.Clear()
-			printDebugCpu(regText, nesEmu)
-			regText.Draw(win, pixel.IM.Scaled(regText.Orig, 1))
-
-			win.Update()
-
-			// Run a CPU instruction everytime the SPACE key is pressed or if autorun
-			// is toggled (triggered by ENTER key).
-			if isAutoRun || win.JustPressed(pixelgl.KeySpace) {
-				cycleChan <- true
-			}
-
-			// Toggle CPU autorun with ENTER key.
-			if win.JustPressed(pixelgl.KeyEnter) {
-				isAutoRun = !isAutoRun
-			}
-		}
-	}
 }
 
 func printDebugMem(t *text.Text, nesEmu *nes.Bus) {
